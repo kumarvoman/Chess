d9c49631b97b8ebe652619cc63062ad975bcda98
kumarvoman <kumarvoman@gmail.com>
2023-02-05 17:59:55 +0530

    Updating chess.cpp file

--- Chess.cpp
+++ Chess.cpp
@@ -2,14 +2,14 @@
 //
 
 #include <iostream>
-#include "Game.h"
 #include <vector>
+#include "Game.h"
 
 using namespace std;
 
 int main()
 {
-    vector<vector<int>> sdf(8, vector<int>(4, 0));
+    //vector<vector<int>> sdf(8, vector<int>(8, 0));
 
     Game* _game = new Game();
     _game->startGame();
